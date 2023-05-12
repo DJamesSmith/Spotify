f90f875ff3e73faee1f9d11952b7be24571d0651
Dion James Smith <dion.smith070@gmail.com>
2023-05-12 12:07:49 +0530

    28. Final Wrap Up: Design the WelcomeScreen UserInterface

--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -4,26 +4,79 @@ class WelcomeViewController: UIViewController {
     
     private let signInButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .white
+        button.backgroundColor = .systemGreen
         button.setTitle("Sign In with Spotify", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        
+        button.layer.shadowColor = UIColor.white.cgColor
+        button.layer.shadowOffset = CGSize(width: 8.0, height: 5.0)
+        button.layer.shadowRadius = 5.0
+        button.layer.shadowOpacity = 0.7
         
         return button
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "albums_background")
+        
+        return imageView
+    }()
+    
+    private let overlayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.65
+        
+        return view
+    }()
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        label.text = "Listen to Millions\nof Songs on\nthe go."
+        
+        return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Spotify"
-        view.backgroundColor = .systemGreen
+        
+        view.addSubview(imageView)
+        view.addSubview(overlayView)
+        view.backgroundColor = .black
         view.addSubview(signInButton)
+        
         signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        
+        view.addSubview(label)
+        view.addSubview(logoImageView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        signInButton.frame = CGRect(x: 20, y: view.height - 50 - view.safeAreaInsets.bottom, width: view.width - 40, height: 50)
+        imageView.frame = view.bounds
+        overlayView.frame = view.bounds
+        
+        signInButton.frame = CGRect(x: 20, y: view.height - 100 - view.safeAreaInsets.bottom, width: view.width - 40, height: 50)
+        
+        logoImageView.frame = CGRect(x: (view.width - 120) / 2, y: (view.height - 350) / 2, width: 120, height: 120)
+        label.frame = CGRect(x: 30, y: logoImageView.bottom + 30, width: view.width - 60, height: 150)
     }
     
     @objc func didTapSignIn() {
